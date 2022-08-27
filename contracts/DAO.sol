94fd854d7aa8483df1163101ccb7cca293be72e2
0zAND1z <0zAND1z.dev@protonmail.com>
2022-08-28 05:09:52 +0530

    Basic DAO Contract

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -1,22 +1,62 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "./TOTE.sol";
+import "./TUSD.sol";
 
-contract Greeter {
-    string private greeting;
+contract ToteDAO {
+    address payable[] users;
+    address payable[] companies;
+    mapping(string => string[]) BountiesAndReviews;
+    mapping(string => uint) HelpfulReviews;
+    mapping(string => address) ReviewAndItsMaker;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    function addNewUser(address payable _newUser)
+        public
+        returns (bool addNewUserStatus)
+    {
+        users.push(_newUser);
+        emit NewUserJoined(_newUser);
+        return true;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    function addNewCompany(address payable _newCompany)
+        public
+        returns (bool addNewCompanyStatus)
+    {
+        companies.push(_newCompany);
+        emit NewCompanyAdded(_newCompany);
+        return true;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function addNewBounty(string memory _bountyHash)
+        public
+        returns (bool addStatus)
+    {
+        emit NewBountyCreated(_bountyHash);
+        return true;
     }
+
+    function addReview(string memory _bountyHash, string memory _reviewHash)
+        public
+        returns (bool statusAddReview)
+    {
+        BountiesAndReviews[_bountyHash].push(_reviewHash);
+        emit NewReviewAdded(_bountyHash, _reviewHash);
+    }
+
+    function addHelpfulReviews(string memory _reviewHash)
+        public
+        returns (bool addHelpfulReviewStatus)
+    {
+        HelpfulReviews[_reviewHash] += 1;
+        emit ReviewHelpful(msg.sender, _reviewHash);
+        return true;
+    }
+
+    event NewUserJoined(address _newUserAddr);
+    event NewCompanyAdded(address _newCompanyAddr);
+    event NewBountyCreated(string _bountyHash);
+    event NewReviewAdded(string _bountyHash, string _reviewHash);
+    event ReviewHelpful(address _user, string _reviewHash);
 }
